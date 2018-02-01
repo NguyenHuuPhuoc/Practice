5a09718fb0d811672f23531ebd99d21ae7422ea6
SKY-20170602ADD\Administrator <phuoc.nguyen@apps-cyclone.com>
2018-02-01 16:19:40 +0700

    feature permutation symmetry in two array

--- Algorithm/Permutations/Implement/Permutation.cs
+++ Algorithm/Permutations/Implement/Permutation.cs
@@ -1,6 +1,7 @@
 ﻿using Algorithm.Commons.Implement;
 using Algorithm.Permutations.Interface;
 using System;
+using System.Linq;
 
 namespace Algorithm.Permutations.Implement
 {
@@ -20,7 +21,7 @@ namespace Algorithm.Permutations.Implement
 
             if (totalElementFirst != totalElementSecond)
                 return false;
-            // Check array permutation
+            // Check element duplicate two array
             for (int i = 0; i < totalElementFirst; i++)
             {
                 if (arrayFirst[i] == arraySecond[i])
@@ -30,18 +31,39 @@ namespace Algorithm.Permutations.Implement
                         return false;
                 }
             }
+            // 
+            if (arrayFirst[(totalElementFirst - 1) / 2] == arraySecond[(totalElementSecond - 1) / 2])
+            {
+                int[] subArrayFirst = arrayFirst.Take(arrayFirst[((totalElementFirst - 1) / 2) - 1]).ToArray();
+                int[] subArraySecond = arraySecond.Take(arraySecond[((totalElementSecond - 1) / 2) - 1]).ToArray();
+                int[] subArrayFirstLast = arrayFirst.Skip(arrayFirst[(totalElementSecond - 1) / 2]).Take(arrayFirst[totalElementSecond - 1]).ToArray();
+                int[] subArraySecondLast = arraySecond.Skip(arraySecond[(totalElementSecond - 1) / 2]).Take(arraySecond[totalElementSecond - 1]).ToArray();
+                Array.Sort(subArrayFirst);
+                Array.Sort(subArraySecond);
+                Array.Sort(subArrayFirstLast);
+                Array.Sort(subArraySecondLast);
+                if (CheckPermutation(subArrayFirst, subArraySecond) && CheckPermutation(subArrayFirstLast, subArraySecondLast))
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
 
-            Array.Sort(arrayFirst);
-            Array.Sort(arraySecond);
+        }
 
-            for (int i = 0; i < totalElementFirst; i++)
+        //Check permutation
+        public bool CheckPermutation(int[] arrayFirst, int[] arraySecond)
+        {
+            for (int i = 0; i < arrayFirst.Length; i++)
             {
                 if (arrayFirst[i] != arraySecond[i])
                     return false;
             }
             return true;
         }
-
         /// <summary>
         /// Main of Permutation
         /// </summary>
