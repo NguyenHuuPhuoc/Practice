2cb66217404de74f8f7cfbf8a87981603ccf7e8a
SKY-20170602ADD\Administrator <phuoc.nguyen@apps-cyclone.com>
2018-02-06 10:16:39 +0700

    reverse code challenge01

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -15,31 +15,9 @@ namespace Algorithm
             //Permutation permutation = new Permutation();
             //permutation.MainPermutation();
 
-            //MostFrequentNumber mostFrequentNumber = new MostFrequentNumber();
-            //mostFrequentNumber.MainMostFrequentNumber();
-            int[] array = new int[10] { 3, 4, 3, 5, 5, 3,5, 1,1,1};
-            int count = 1, tempCount;
-            int frequentNumber = array[0];
-            int tempNumber = 0;
-            for (int i = 0; i < (array.Length - 1); i++)
-            {
-                tempNumber = array[i];
-                tempCount = 0;
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (tempNumber == array[j])
-                    {
-                        tempCount++;
-                    }
-                }
-                if (tempCount > count)
-                {
-                    frequentNumber = tempNumber;
-                    count = tempCount;
-                }
-            }
-            Console.WriteLine("The most frequent number in this array is {0} has been repeated {1} times.", frequentNumber, count);
-            Console.ReadKey();
+            MostFrequentNumber mostFrequentNumber = new MostFrequentNumber();
+            mostFrequentNumber.MainMostFrequentNumber();
+
         }
     }
 }
