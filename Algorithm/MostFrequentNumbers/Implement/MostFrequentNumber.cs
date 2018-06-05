d7bf0663b64b3d0c4faf7b70d03769a7e8b4d1ee
PHUOCNH\PHUOC <phuoc.nguyenhuu@haravan.com>
2018-06-05 22:37:47 +0700

    fix

--- Algorithm/MostFrequentNumbers/Implement/MostFrequentNumber.cs
+++ Algorithm/MostFrequentNumbers/Implement/MostFrequentNumber.cs
@@ -73,28 +73,28 @@ namespace Algorithm.MostFrequentNumbers.Implement
             return max;
         }
 
-        //public int getPopularElement(int[] a)
-        //{
-        //    int count = 1, tempCount;
-        //    int popular = a[0];
-        //    int temp = 0;
-        //    for (int i = 0; i < (a.Length-1); i++)
-        //    {
-        //        temp = a[i];
-        //        tempCount = 0;
-        //        for (int j = 1; j < a.Length; j++)
-        //        {
-        //            if (temp == a[j])
-        //                tempCount++;
-        //        }
-        //        if (tempCount > count)
-        //        {
-        //            popular = temp;
-        //            count = tempCount;
-        //        }
-        //    }
-        //    return popular;
-        //}
+        public int getPopularElement(int[] a)
+        {
+            int count = 1, tempCount;
+            int popular = a[0];
+            int temp = 0;
+            for (int i = 0; i < (a.Length - 1); i++)
+            {
+                temp = a[i];
+                tempCount = 0;
+                for (int j = 1; j < a.Length; j++)
+                {
+                    if (temp == a[j])
+                        tempCount++;
+                }
+                if (tempCount > count)
+                {
+                    popular = temp;
+                    count = tempCount;
+                }
+            }
+            return popular;
+        }
 
         /// <summary>
         /// Business
